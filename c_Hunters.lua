cebe5729fa0bb70de8630e9e55f347ac2ecdd90f
LucasSangino <Lucas.Sangino@alu.frt.utn.edu.ar>
2025-05-06 12:07:27 -0300

    cambios realizados

--- c_Hunters.lua
+++ c_Hunters.lua
@@ -1,13 +1,5 @@
 
-local Hunter = createPed(33,-1628.8681640625, -2239.294921875,31.4765625)
 
-    setPedRotation(Hunter,90)
-    setElementHealth(Hunter, 100)
-    setPedArmor(Hunter, 100)
-    setElementFrozen(Hunter, true) -- Evita que el NPC se mueva solo
-    setElementData(Hunter, "npc", true) -- Identificar como NPC
-
-    
 
 
 marcador = createMarker ( -1630.025390625, -2239.4326171875, 30 ,"cylinder", 1.0,255, 162, 51, 255 )
