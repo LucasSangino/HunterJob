595e5b4804dab9ccc4e9ac450e720e36c60b5e45
LucasSangino <Lucas.Sangino@alu.frt.utn.edu.ar>
2025-05-06 12:30:12 -0300

    operacion principal

--- c_Hunters.lua
+++ c_Hunters.lua
@@ -1,6 +1,8 @@
 
 local Hunter = createPed(33,-1628.8681640625, -2239.294921875,31.4765625)
 
+
+//se agrego un Huntermanager
     setPedRotation(Hunter,90)
     setElementHealth(Hunter, 100)
     setPedArmor(Hunter, 100)
